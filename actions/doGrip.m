94b0e21a1e1f08172f5c767f0dc28e4d569a00e2
remurdock <remurdock@mail.lipscomb.edu>
2024-04-02 15:23:46 -0500

    saving altered do grip file

--- actions/doGrip.m
+++ actions/doGrip.m
@@ -15,7 +15,7 @@ function [res,state] = doGrip(type)
     grip_msg = rosmessage(grip_action_client);
 
     %% Set Grip Pos by default to pick / close gripper
-    gripPos = 0.225; 
+    gripPos = 0.23; 
 
     % Modify it if place (i.e. open)
     if strcmp(type,'place')
